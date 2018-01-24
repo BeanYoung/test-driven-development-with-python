5f81bc1ad3747c0ccd0874b6a249a8e2230f0bc4
BeanYoung <beanyoung.cn@gmail.com>
2018-01-24 19:12:37 +0800

    add templates

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -3,6 +3,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,7 +17,19 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
+
+        inputbox.send_keys('Buy xbox one x')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy xbox one x' for row in rows))
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
