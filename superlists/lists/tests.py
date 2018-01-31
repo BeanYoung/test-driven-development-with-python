cd7f324963c9b3c82f1fe96a9a1d84eef10fc193
BeanYoung <beanyoung.cn@gmail.com>
2018-01-31 19:47:20 +0800

    add POST redirect and home display list

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -25,4 +26,51 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='One')
+        Item.objects.create(text='Two')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('One', response.content.decode())
+        self.assertIn('Two', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'First item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(second_saved_item.text, 'Second item')
