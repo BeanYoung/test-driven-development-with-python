1140b7253a776798cec655c18f861057faf612ba
BeanYoung <beanyoung.cn@gmail.com>
2018-01-31 15:50:08 +0800

    add home page post

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -27,9 +32,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy xbox one x')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy xbox one x' for row in rows))
+        self.check_for_row_in_list_table('1: Buy xbox one x')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
